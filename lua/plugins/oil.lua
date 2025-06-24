a352b2069db7f875c55d4f2a42c455b3f51d242f
DCPR01 <162784589+DCPR01@users.noreply.github.com>
2025-06-24 12:37:07 -0400

    Update oil.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,26 +1,28 @@
 return {
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional for file icons
-        config = function()
-            require("oil").setup({
-                default_file_explorer = true, -- Replaces netrw
-                columns = { "icon" }, -- Show file icons
-                keymaps = {
-                    ["<CR>"] = "actions.select", -- Open file or enter directory
-                    ["<C-v>"] = "actions.select_vsplit", -- Open in vertical split
-                    ["<C-x>"] = "actions.select_split", -- Open in horizontal split
-                    ["<C-t>"] = "actions.select_tab", -- Open in new tab
-                    ["<BS>"] = "actions.parent", -- Go up a directory
-                    ["q"] = "actions.close", -- Close oil.nvim
-                },
-                view_options = {
-                    show_hidden = true, -- Show hidden files
-                },
-            })
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional for file icons
+  config = function()
+    local oil = require("oil")
+    local actions = require("oil.actions")
 
-            -- ✅ Keybinding to Open Oil with Ctrl+O
-            vim.keymap.set("n", "<C-o>", ":Oil<CR>", { noremap = true, silent = true })
-        end
-    }
+    oil.setup({
+      default_file_explorer = true, -- Replaces netrw
+      columns = { "icon" },         -- Show file icons
+      view_options = {
+        show_hidden = true,         -- Show hidden files
+      },
+      keymaps = {
+        ["<CR>"]    = "actions.select",           -- Open file/directory
+        ["<C-v>"]   = "actions.select_vsplit",    -- Vertical split
+        ["<C-x>"]   = "actions.select_split",     -- Horizontal split
+        ["<C-t>"]   = "actions.select_tab",       -- New tab
+        ["<BS>"]    = "actions.parent",           -- Go up one directory
+        ["q"]       = "actions.close",            -- Close Oil
+        ["<C-CR>"]  = actions.cd.callback,        -- ✅ CD to dir
+      },
+    })
+
+    -- Global keymap to open Oil from anywhere with Ctrl+o
+    vim.keymap.set("n", "<C-o>", "<cmd>Oil<CR>", { noremap = true, silent = true })
+  end,
 }
