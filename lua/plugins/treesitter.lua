0ac8cf4de936a7451c8a1fad362c93a55b024102
DCPR01 <162784589+DCPR01@users.noreply.github.com>
2025-06-27 21:27:45 -0400

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = "all",
-            auto_install = false,
-            highlight = { enable = true },
-            indent = { enable = true }
-        })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+      ensure_installed = {
+        "lua",
+        "python",
+        "bash",
+        "html",
+        "c",
+        "cpp",
+        "yaml",
+        "dockerfile"
+      },
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true }
+    })
+  end
 }
-
--- return {
---   "nvim-treesitter/nvim-treesitter",
---   build = ":TSUpdate",
---   config = function()
---     require("nvim-treesitter.configs").setup {
---       ensure_installed = "all",
---       highlight = { enable = true },
---       indent = { enable = true },
---     }
---   end
--- }
